39377eda89bf2005602d872e653655561bdb53a6
Syauqy Nurul Aziz <syauqyaziz@gmail.com>
2021-07-05 23:12:07 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env:{
-    AIRTABLE_API_KEY: `keym2E3xKv1CV3xJo`,
-    AIRTABLE_TABLE_ID: `oksigen`,
-    AIRTABLE_BASE_ID: `appdfPcan1cl8FjrI`
-  }
-}
